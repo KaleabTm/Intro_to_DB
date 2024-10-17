1acea94e6f22e8774f6aa371ce1ca03e8cc36c51
KaleabTm <kaltamiru8@gmail.com>
2024-10-18 01:32:30 +0300

    pest

--- task_2.sql
+++ task_2.sql
@@ -5,7 +5,54 @@ CREATE TABLE IF NOT EXISTS Authors (
     bio TEXT
 );
 
--- Create table for books
+-- Create table for books-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the newly created database
+USE alx_book_store;
+
+-- Create Authors table
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create Books table
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create Customers table
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT NOT NULL
+);
+
+-- Create Orders table
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -20,8 +67,8 @@ CREATE TABLE IF NOT EXISTS Books (
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone VARCHAR(15)
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
 );
 
 -- Create table for orders
--- task_4.sql
+++ task_4.sql
@@ -2,4 +2,16 @@
 USE alx_book_store;
 
 -- Get the full description of the books table
-SELECT  * FROM information_schema.COLUMNS WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH AS MAX_LENGTH,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_TYPE,
+    EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_NAME = 'books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
