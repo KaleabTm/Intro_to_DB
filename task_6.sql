db1cf0d11e1eb2873c7fd968ed3b797ccb232cac
KaleabTm <kaltamiru8@gmail.com>
2024-10-18 01:44:18 +0300

    all

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,10 @@
--- Use the specified database
+-- task_6.sql
+
 USE alx_book_store;
 
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_name, email, address) 
-VALUES 
-    ( 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."),
-    ( 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    ( 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
